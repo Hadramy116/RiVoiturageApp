251e225e36716b443adf0f93cf1b636146ff26e8
EL Hadramy Dhmine <hadramy116@gmail.com>
2019-08-08 13:39:31 +0000

    heroku setting

--- CoVoiturage/settings.py
+++ CoVoiturage/settings.py
@@ -5,7 +5,7 @@ import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-django_heroku.settings(locals())
+
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -110,3 +110,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+django_heroku.settings(locals())
